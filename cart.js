01687ac93fba624c78deb923d57fe2ccaa97c7d9
Andréa <andreaepivent>
2024-04-16 16:51:21 +0200

    add validate button and routes

--- cart.js
+++ cart.js
@@ -1,43 +1,89 @@
-fetch("http://localhost:3000/cart/allCart")
-  .then((response) => response.json())
-  .then((travelCart) => {
-    if (travelCart) {
-      // Initialisation de la div
-      document.querySelector("#test").textContent = "";
+// Fonction pour afficher la liste des trajets
+function displayTravelsInCart() {
+  fetch("http://localhost:3000/cart/allCart")
+    .then((response) => response.json())
+    .then((travelCart) => {
+      // S'il y a des trajets dans le panier
+      if (travelCart.result) {
+        // Initialisation de la div
+        const testDiv = document.querySelector("#test");
+        testDiv.textContent = "";
 
-      for (let travel of travelCart.travels) {
-        console.log(travel);
+        for (let travel of travelCart.travels) {
+          console.log(travel);
 
-        // Récupérer l'heure
-        const date = new Date(travel.travelInfos.date);
-        const heure = date.getHours();
-        const minutes = date.getMinutes();
+          // Récupérer l'heure
+          const date = new Date(travel.travelInfos.date);
+          const heure = date.getHours().toString().padStart(2, "0");
+          const minutes = date.getMinutes().toString().padStart(2, "0");
 
-        // Ajout des voyages dans des divs individuelles
-        document.querySelector(
-          "#test"
-        ).innerHTML += `<div><p>${travel.travelInfos.departure} > ${travel.travelInfos.arrival}</p>
-            <p>${heure}:${minutes}</p>
-            <p class="prix">${travel.travelInfos.price}€</p>
-            <p>boutton</p></div>`;
+          // Ajout des voyages dans des divs individuelles
+          testDiv.innerHTML += `<div><p>${travel.travelInfos.departure} > ${travel.travelInfos.arrival}</p>
+                <p>${heure}:${minutes}</p>
+                <p class="prix">${travel.travelInfos.price}€</p>
+                <p class="delete-travel" id="${travel.travelInfos._id}" >bouton</p></div>`;
+        }
+
+        // Calcul du total et ajout à la div
+        let prix = document.querySelectorAll(".prix");
+        let total = 0;
+        for (let p of prix) {
+          let prixTravel = Number(p.textContent.replace("€", ""));
+          total += prixTravel;
+        }
+        testDiv.innerHTML += `<div><p>Total: ${total}€</p> <p id="purchase">Purchase</p></div>`;
       }
 
-      // On ajoute une div pour le total
-      let prix = document.querySelectorAll(".prix");
-      let total = 0;
-      for (let p of prix) {
-        let prixTravel = Number(p.textContent.replace("€", ""));
-        total += prixTravel;
+      // Sinon
+      else {
+        // Initialisation de la div
+        document.querySelector("#test").innerHTML = `<p>No booking yet.</p>
+            <p>Why not plan a trip?</p>`;
       }
-      document.querySelector(
-        "#test"
-      ).innerHTML += `<div><p>Total: ${total}€</p> <p>Purchase</p></div>`;
-    } else {
-      // Initialisation de la div
-      document.querySelector("#test").textContent = "";
 
-      // Ajout du message d'erreur
-      document.querySelector("#test").innerHTML += `<p>No booking yet.</p>
-        <p>Why not plan a trip?</p>`;
-    }
-  });
+      // On ajoute un écouteur sur chaque bouton delete
+      deleteButtons = document.querySelectorAll(".delete-travel");
+      for (let button of deleteButtons) {
+        button.addEventListener("click", () => {
+          let id = button.getAttribute("id");
+          fetch(`http://localhost:3000/cart/deleteTrip/${id}`, {
+            method: "DELETE",
+          })
+            .then((response) => response.json())
+            .then(() => {
+              console.log("Travel successfully deleted");
+              // Rafraîchir la liste des trajets après la suppression
+              displayTravelsInCart();
+            });
+        });
+      }
+
+      // On ajoute un écouteur sur le bouton purchase
+      document.querySelector("#purchase").addEventListener("click", () => {
+        // On transfère à bookings
+        fetch("http://localhost:3000/bookings/purchase", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        })
+          .then((response) => response.json())
+          .then(() => {
+            console.log("Cart successfully transfered to Bookings");
+          });
+
+        // On supprime le panier
+        fetch("http://localhost:3000/cart/emptyCart", {
+          method: "DELETE",
+        })
+          .then((response) => response.json())
+          .then(() => {
+            console.log("Cart successfully deleted");
+          });
+
+        // On redirige vers booking.html
+        window.location.assign("booking.html");
+      });
+    });
+}
+
+// Appel de la fonction pour afficher la liste des trajets
+displayTravelsInCart();
