75a8770f8da8dbc314e55a8d44673974baa214cc
kameamea133 <monnier1977@gmail.com>
2024-04-17 15:20:43 +0200

    push final page

--- booking.js
+++ booking.js
@@ -1,14 +1,17 @@
 fetch("http://localhost:3000/bookings/allBookings")
   .then((response) => response.json())
   .then((travelBooking) => {
-    // Initialisation de la div
-    const testDiv = document.querySelector("#test");
-    testDiv.textContent = "";
-
     // S'il y a des trajets dans booking
     console.log(travelBooking);
     if (travelBooking.result) {
+
+         // Ajout du header
+         document.querySelector("#myBookings").innerHTML = 
+         `<p4 id="myBookingsLineOne">My bookings</p4>`;
+
       for (let travel of travelBooking.travels) {
+
+
         // Récupérer l'heure
         const date = new Date(travel.travelInfos.date);
         const heure = date.getHours().toString().padStart(2, "0");
@@ -19,7 +22,7 @@ fetch("http://localhost:3000/bookings/allBookings")
         const difference = Math.floor((date - dateNow) / (1000 * 60 * 60)); // différence en heures
 
         // Ajout des voyages dans des divs individuelles
-        testDiv.innerHTML += `<div>
+        document.querySelector("#myBookings").innerHTML += `<div class="trip">
         <p>${travel.travelInfos.departure} > ${travel.travelInfos.arrival}</p>
                     <p>${heure}:${minutes}</p>
                     <p>${travel.travelInfos.price}€</p>
@@ -29,9 +32,13 @@ fetch("http://localhost:3000/bookings/allBookings")
                     
       }
 
-      testDiv.innerHTML += `<div><p>Enjoy your travels with Tickethack!</p></div>`;
+      document.querySelector("#myBookings").innerHTML += 
+      `<div class="underlineBar"></div>
+      <div><p class="congratMessage">Enjoy your travels with Tickethack!</p></div>
+      `;
+
     } else {
-      document.querySelector("#test").innerHTML = `<p>No booking yet.</p>
+      document.querySelector("#myBookings").innerHTML = `<p>No booking yet.</p>
             <p>Why not plan a trip?</p>`;
     }
   });
